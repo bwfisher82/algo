ded97aa08cb8d246b026bb92e2e86015779ef44b
Ben Fisher <kurasoe@gmail.com>
2022-04-30 05:16:41 -0500

    add test cases from algoexperts.io

--- src/easy/03_sorted_squared_array/test_sorted_squared_array.py
+++ src/easy/03_sorted_squared_array/test_sorted_squared_array.py
@@ -1,39 +1,13 @@
 import pytest
 
-from random import randint
-
 from main import optimal, suboptimal
 
 
 class TestSortedSquaredArray:
-    @classmethod
-    def _generate_input(cls, input_type=None):
-        if input_type is None:
-            input_type = randint(1, 10)
-        if input_type == 1:
-            return [randint(-20, 20) for i in range(20)]
-        elif input_type == 2:
-            return []
-        elif input_type == 3:
-            return [randint(0, 20) for i in range(10)]
-        elif input_type == 4:
-            return [randint(-20, 20) for i in range(500)]
-        elif input_type == 5:
-            return [randint(-100, 100) for i in range(100)]
-        elif input_type == 6:
-            return [randint(-2, 2) for i in range(2)]
-        elif input_type == 7:
-            return [randint(-1000, 1000) for i in range(20)]
-        elif input_type == 8:
-            return [randint(-50, 50) for i in range(1_000)]
-        elif input_type == 9:
-            return ""
-        elif input_type == 10:
-            return [randint(-1000, 1000) for i in range(1_000)]
 
     @classmethod
     def _is_result_sorted(cls):
-        array = TestSortedSquaredArray._generate_input(1)
+        array = [-7, -3, 1, 9, 22, 30]
         result_optimal = optimal(sorted(array))
         result_suboptimal = suboptimal(sorted(array))
         return result_optimal == sorted(result_optimal) and \
@@ -45,7 +19,23 @@ class TestSortedSquaredArray:
 
     @pytest.mark.parametrize("array, expected_result", [
         ([1, 2, 3, 5, 6, 8, 9], [1, 4, 9, 25, 36, 64, 81]),
-        ([-12, -4, -1, 0, 1, 5, 10, 13], [0, 1, 1, 16, 25, 100, 144, 169]),
+        ([1], [1]),
+        ([1, 2], [1, 4]),
+        ([1, 2, 3, 4, 5], [1, 4, 9, 16, 25]),
+        ([0], [0]),
+        ([10], [100]),
+        ([-1], [1]),
+        ([-2, -1], [1, 4]),
+        ([-5, -4, -3, -2, -1], [1, 4, 9, 16, 25]),
+        ([-10], [100]),
+        ([-10, -5, 0, 5, 10], [0, 25, 25, 100, 100]),
+        ([-7, -3, 1, 9, 22, 30], [1, 9, 49, 81, 484, 900]),
+        ([-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20],
+            [0, 0, 1, 1, 1, 4, 4, 9, 169, 361, 400, 2500]),
+        ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ([-1, -1, 2, 3, 3, 3, 4], [1, 1, 4, 9, 9, 9, 16]),
+        ([-3, -2, -1], [1, 4, 9]),
+        ([-3, -2, -1], [1, 4, 9]),
         ([], [])
     ])
     def test_given_arrays_return_valid_results(self, array, expected_result):
--- src/easy/05_non_constructible_change/test_non_constructible_change.py
+++ src/easy/05_non_constructible_change/test_non_constructible_change.py
@@ -1,59 +1,23 @@
-import random
 import pytest
-
-from enum import Enum
 
 from main import non_constructible_change
 
 
 class TestNonBuildingChange:
-    @classmethod
-    def _get_sample_coins(cls) -> list:
-        return [5, 7, 1, 1, 2, 3, 22]
-
-    @classmethod
-    def _get_coins_small_size(cls) -> list:
-        return [29, 33, 19, 37, 5, 43, 27, 29, 35, 47, 3, 28, 11, 36, 45, 3, 8, 14, 33, 11,
-                28, 4, 21, 29, 24, 33, 21, 32, 35, 5, 2, 9, 4, 14, 2, 42, 11, 39, 21, 41, 49,
-                48, 2, 33, 14, 20, 1, 4, 23, 5]
-
-    @classmethod
-    def _get_coins_medium_size(cls) -> list:
-        return cls._get_coins_small_size() * 200
-
-    @classmethod
-    def _get_coins_large_size(cls) -> list:
-        return cls._get_coins_medium_size() * 25
-
-    def test_no_change_returns_one(self):
-        assert non_constructible_change([]) == 1
-
-    def test_change_of_1_returns_2(self):
-        assert non_constructible_change([1]) == 2
-
-    def test_change_of_1_2_returns_4(self):
-        assert non_constructible_change([1, 2]) == 4
-
-    def test_change_of_1_2_3_returns_7(self):
-        assert non_constructible_change([1, 2, 3]) == 7
-
-    def test_change_of_3_2_1_returns_7(self):
-        assert non_constructible_change([3, 2, 1]) == 7
-
-    def test_change_using_sample_coins(self):
-        assert non_constructible_change(self.__class__._get_sample_coins()) == 20
-
-    @pytest.mark.parametrize("size, expected_result", [
-        ("small", 1113),
-        ("medium", 222401),
-        ("large", 5560001)
+    @pytest.mark.parametrize("coins, expected_result", [
+        ([5, 7, 1, 1, 2, 3, 22], 20),
+        ([1, 1, 1, 1, 1], 6),
+        ([1, 5, 1, 1, 1, 10, 15, 20, 100], 55),
+        ([6, 4, 5, 1, 1, 8, 9], 3),
+        ([], 1),
+        ([87], 1),
+        ([5, 6, 1, 1, 2, 3, 4, 9], 32),
+        ([5, 6, 1, 1, 2, 3, 43], 19),
+        ([1, 1], 3),
+        ([2], 1),
+        ([1], 2),
+        ([109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4], 87),
+        ([1, 2, 3, 4, 5, 6, 7], 29)
     ])
-    def test_larger_coin_sizes(self, size: str, expected_result: int):
-        coins = []
-        if size == "small":
-            coins = self.__class__._get_coins_small_size()
-        elif size == "medium":
-            coins = self.__class__._get_coins_medium_size()
-        elif size == "large":
-            coins = self.__class__._get_coins_large_size()
+    def test_larger_coin_sizes(self, coins: list, expected_result: int):
         assert non_constructible_change(coins) == expected_result
